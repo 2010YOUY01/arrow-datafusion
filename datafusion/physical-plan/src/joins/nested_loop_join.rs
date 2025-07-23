639a3c37181523774f2bb5c3f6e811aa6c586bf3
Yongting You <2010youy01@gmail.com>
2025-07-23 10:49:51 +0800

    Init NLJ rewrite

--- datafusion/physical-plan/src/joins/nested_loop_join.rs
+++ datafusion/physical-plan/src/joins/nested_loop_join.rs
@@ -539,6 +539,7 @@ impl ExecutionPlan for NestedLoopJoinExec {
         };
 
         if USE_NLJ_V2 {
+            println!("Using NLJ v2");
             Ok(Box::pin(NLJStream::new(
                 self.schema(),
                 self.filter.clone(),
